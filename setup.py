e2f7038bda856f3a9c30cf2ea76f71655ee90bc9
Peter Stein <peterstein@protonmail.com>
2020-03-28 23:20:21 +0100

    Read version from __init__.py

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
+import codecs
+import os
+import re
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), "r") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name="apocarich",
-    version="0.0.1",
+    version=find_version("apocarich", "__init__.py"),
     url="https://github.com/ptrstn/apocarich",
+    author="Peter Stein",
     packages=find_packages(),
-    install_requires=[
-        "requests",
-        "pandas",
-        "awscli",
-        "plotly",
-        "matplotlib",
-        "seaborn",
-    ],
+    install_requires=["requests", "pandas", "awscli",],
     entry_points={"console_scripts": ["apocarich=apocarich.__main__:main",]},
     scripts=["R/apocarich.R"],
 )
