363fe21490bf9ccff6abceb5f36d7a608c68dfb3
Peter Stein <peterstein@protonmail.com>
2020-03-28 22:43:55 +0100

    Bump version 0.1.0

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ setup(
     version="0.0.1",
     url="https://github.com/ptrstn/apocarich",
     packages=find_packages(),
-    install_requires=["requests", "pandas", "awscli", "plotly", "matplotlib", "seaborn"],
-    entry_points={
-        "console_scripts": [
-            "apocarich=apocarich.__main__:main",
-        ]
-    },
-    scripts=['R/apocarich.R'],
+    install_requires=[
+        "requests",
+        "pandas",
+        "awscli",
+        "plotly",
+        "matplotlib",
+        "seaborn",
+    ],
+    entry_points={"console_scripts": ["apocarich=apocarich.__main__:main",]},
+    scripts=["R/apocarich.R"],
 )
